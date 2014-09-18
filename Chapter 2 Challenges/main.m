e45ef4a9e364f5f3fd4189f06021092ad72ba39b
Albert Saucedo <saucedoa7@gmail.com>
2014-09-18 18:49:10 -0500

    Initial Commit

---
+++ Chapter 2 Challenges/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Chapter 2 Challenges
+//
+//  Created by Albert Saucedo on 9/18/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
